af19f552079bb876e3202b9ed83eb5626d5d935c
Pushpendra Sharma <122333300+pushpendra010@users.noreply.github.com>
2024-03-31 20:33:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+//selecting elements 
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//rolling dice functionality
+
+btnRoll.addEventListener('click', function() {
+    const dice =Math.trunc(Math.random() * 6) +1;
+    console.log(dice);
+    diceEl.classList.remove('hidden');
+    diceEl.src =`dice-${dice}.png`;
+
+    //check for rolled is 1 or not
+if(dice !=1){
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; 
+}else{
+    //switch to next player
+
+}
+}
+);
+
+
+
+
