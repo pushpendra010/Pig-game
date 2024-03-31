ac9b0d55d5863e61d76c08e4be2a26150192cc66
Pushpendra Sharma <122333300+pushpendra010@users.noreply.github.com>
2024-03-31 22:02:49 +0530

    ❤️❤️

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //starting conditions
 score0El.textContent = 0;
@@ -28,10 +32,18 @@ btnRoll.addEventListener('click', function() {
 if(dice !=1){
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; 
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    
 }else{
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    
 
+    
 }
 }
 );
