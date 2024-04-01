2854cd7538a5884793107aa3ac77fc2c8ec03fa8
Pushpendra Sharma <122333300+pushpendra010@users.noreply.github.com>
2024-04-01 20:32:52 +0530

    😊😊

--- script.js
+++ script.js
@@ -14,15 +14,27 @@ const btnHold = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+ let playing = true;
+
+
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const switchPlayer = function (){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
 //rolling dice functionality
 
 btnRoll.addEventListener('click', function() {
+    if (playing) {
     const dice =Math.trunc(Math.random() * 6) +1;
     console.log(dice);
     diceEl.classList.remove('hidden');
@@ -36,28 +48,42 @@ if(dice !=1){
     
 }else{
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
     
 
     
-}
+}}
 }
 );
 
 btnHold.addEventListener('click',function(){
+    if(playing){
    //1. add current score to active player
    scores[activePlayer] += currentScore;
+   
    //scores[1] = scores[1]+currentScore;
-   document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
+   document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
    //2. check if player's score is 100 or not
+
+   if(scores[activePlayer] >= 10){
+    //Finish the game
+    playing = false;
+    diceEl.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+    
+   }else{
+    //3. switch to next player
+    switchPlayer();
+   }
    
-   //3. switch to next player
+    
+}
+   
 });
+
 
 
 
